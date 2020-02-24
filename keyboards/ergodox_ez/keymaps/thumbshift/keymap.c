0811b791a69bee51dfdd5b96a7eee536e3abaf41
Massn <massn23579@gmail.com>
2020-02-24 15:16:44 +0900

    :hammer: update directions-mapping

--- keyboards/ergodox_ez/keymaps/thumbshift/keymap.c
+++ keyboards/ergodox_ez/keymaps/thumbshift/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|LGui+ |           |Alt   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LAlt  |           |+SPC  |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf|   {  |   (  |                                       |   )  |   }  |   ~  |      | ~L1  |
+ *   |Grv/L1|  '"  |   (  | Down | Left |                                       | Right|  Up  |   )  |  ~   | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | Home |       | PgUp  |Ctrl/Esc|
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_HENK,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LGUI(KC_LALT),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  LSFT(KC_LBRC),  KC_LPRN,
+        LT(SYMB,KC_GRV),KC_QUOT,      KC_LPRN,KC_DOWN,KC_LEFT,
                                               ALT_T(KC_APP),  KC_HOME,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_LGUI,
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,      KC_P,             KC_BSLS,
                           KC_H,   KC_J,  KC_K,   KC_L,      KC_SCLN,          GUI_T(KC_QUOT),
          LALT(KC_SPC),    KC_N,   KC_M,  KC_COMM,KC_DOT,    KC_SLSH,          KC_RSFT,
-                                  KC_RPRN,LSFT(KC_RBRC), LSFT(KC_GRV),     KC_NO,       KC_FN1,
+                                  KC_RIGHT,KC_UP,KC_RPRN,   LSFT(KC_GRV),      KC_FN1,
              KC_PGUP,        CTL_T(KC_ESC),
              KC_PGDN,
              KC_LALT,KC_TAB, KC_ENT
--- update_thumbshift.sh
+++ update_thumbshift.sh
@@ -2,4 +2,5 @@
 
 make ergodox_ez:thumbshift
 echo "start teensy"
-# teensy_loader_cli -mmcu=atmega32u4 -w ergodox_ez_thumbshift.hex
+
+# flash your keboard using wally
