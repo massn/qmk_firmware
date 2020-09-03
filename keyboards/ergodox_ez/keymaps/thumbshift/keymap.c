a96e6f1dc68d09c95e7399cab6cc75f2d2f00eb1
Massn <massn23579@gmail.com>
2020-09-03 13:32:46 +0900

    :hammer:

--- keyboards/ergodox_ez/keymaps/thumbshift/keymap.c
+++ keyboards/ergodox_ez/keymaps/thumbshift/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|LGui+ |           |Alt   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |LAlt  |           |+SPC  |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |   [  | Down | Left |                                       | Right|  Up  |   )  |  ~   | ~L1  |
+ *   |Grv/L1|  '"  | Down | Left |  [   |                                  | ]  | Right|  Up  |  ~   | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | Home |       | PgUp  |Ctrl/Esc|
@@ -61,16 +61,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_HENK,
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LGUI(KC_LALT),
-        LSFT(KC_GRV),   KC_QUOT,      KC_LBRC,KC_DOWN,KC_LEFT,
+        LSFT(KC_GRV),   KC_QUOT,      KC_DOWN,KC_LEFT,KC_LBRC,
                                               ALT_T(KC_APP),  KC_HOME,
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_LGUI,
         // right hand
-             KC_RPRN,     KC_6,   KC_7,  KC_8,   KC_9,      KC_0,             KC_MINS,
-             TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,      KC_P,             KC_BSLS,
-                          KC_H,   KC_J,  KC_K,   KC_L,      KC_SCLN,          GUI_T(KC_QUOT),
-         LALT(KC_SPC),    KC_N,   KC_M,  KC_COMM,KC_DOT,    KC_SLSH,          KC_RSFT,
-                                  KC_RIGHT,KC_UP,KC_RBRC,   LT(SYMB,KC_GRV),  KC_FN1,
+        KC_RPRN,          KC_6,   KC_7,     KC_8,   KC_9,             KC_0,    KC_MINS,
+        TG(SYMB),         KC_Y,   KC_U,     KC_I,   KC_O,             KC_P,    KC_BSLS,
+                          KC_H,   KC_J,     KC_K,   KC_L,             KC_SCLN, GUI_T(KC_QUOT),
+        LALT(KC_SPC),     KC_N,   KC_M,     KC_COMM,KC_DOT,           KC_SLSH, KC_RSFT,
+                          KC_RBRC,KC_RIGHT, KC_UP,  LT(SYMB,KC_GRV),  KC_FN1,
              KC_PGUP,        CTL_T(KC_ESC),
              KC_PGDN,
              KC_LALT,KC_TAB, KC_ENT
